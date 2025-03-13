50f1bac91c17345bc6a4756cd054bcfeb62840c0
unknown <tarakayalakala@gmail.com>
2025-03-13 14:46:24 +0530

    Confilct resolved

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -61,7 +61,7 @@ function Home() {
             About me
           </div>
           <div className="info-details">
-            Hi, I am <span style={{fontSize:"40px",color:"rgb(173, 94, 247)",fontWeight:"750"}}>Taraka</span>
+            Hi,I am <span style={{fontSize:"40px",color:"rgb(173, 94, 247)",fontWeight:"750"}}>Taraka</span>
           </div>
            <div className="info-sa">A passionate Full stack developer</div>
            <p className='para-abt'>With good command in Desgin and Development,  I specialize in crafting engaging and responsive web applications using MongoDB, Express.js, React.js, and Node.js.</p>
